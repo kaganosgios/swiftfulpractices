84e6d5c00061c621b8c4815de2729bbe113c06c9
kaganosgios <kagankaplan1925@gmail.com>
2026-02-05 21:36:53 +0300

    Initial Commit

---
+++ AuthStoreMini/AuthStoreMiniApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthStoreMiniApp.swift
+//  AuthStoreMini
+//
+//  Created by KağanKAPLAN on 5.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct AuthStoreMiniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AuthStoreMiniTests/AuthStoreMiniTests.swift
@@ -0,0 +1,16 @@
+//
+//  AuthStoreMiniTests.swift
+//  AuthStoreMiniTests
+//
+//  Created by KağanKAPLAN on 5.02.2026.
+//
+
+import Testing
+
+struct AuthStoreMiniTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
